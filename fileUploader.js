6e74638d3b34a685f711bd805ca4e9ce11eaea7b
James B <jvb.codes@gmail.com>
2024-08-07 16:06:32 +0900

    fetch updated

--- fileUploader.js
+++ fileUploader.js
@@ -69,7 +69,7 @@ const sendImgs = async (images) => {
   });
 
   try {
-    const response = await fetch("http://localhost:3000/upload", {
+    const response = await fetch("http://192.168.3.3:3000/upload", {
       method: "POST",
       body: formData,
     });
@@ -86,6 +86,7 @@ const sendImgs = async (images) => {
       imgNames.append(responseMessage);
     }
   } catch (error) {
+    console.log(error);
     errorMsg.textContent = error.message;
     errorMsg.style.color = "red";
     uploaderContainer.append(errorMsg);
